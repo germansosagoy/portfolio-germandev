7fbc5fb54a55f28cbaa82eb1f736f167591294cc
germansosagoy <germanesosagoy@gmail.com>
2024-05-08 13:02:34 -0300

    feat & refactor: add Medium icon

---
+++ src/lib/constants.ts
@@ -0,0 +1,116 @@
+// import type { Site, Page } from "./types";
+
+// export const loader = [
+//   ".loader",
+//   { opacity: [1, 0], pointerEvents: "none" },
+//   { easing: "ease-out" },
+// ];
+
+// export const LINKS = {
+//   github: "https://github.com/germansosagoy",
+//   linkedin: "https://www.linkedin.com/in/german-sosa-goy-/",
+//   mail: "mailto:germanesosagoy@gmail.com",
+//   //   instagram: 'https://www.instagram.com/ancap.tattoo/',
+//   medium: "https://medium.com/@germanesosagoy",
+//   //   discord: 'https://discordapp.com/users/163300027618295808',
+// };
+
+// // // Global
+// // export const SITE: Site = {
+// //   title: 'Astro Sphere',
+// //   description:
+// //     'Welcome to Astro Sphere, a portfolio and blog for designers and developers.',
+// //   author: 'Mark Horn',
+// // }
+
+// // Work Page
+// export const WORK: Page = {
+//   title: "Work",
+//   description: "Places I have worked.",
+// };
+
+// // Blog Page
+// export const BLOG: Page = {
+//   title: "Blog",
+//   description: "Writing on topics I am passionate about.",
+// };
+
+// // Projects Page
+// export const PROJECTS: Page = {
+//   title: "Projects",
+//   description: "Recent projects I have worked on.",
+// };
+
+// // Search Page
+// export const SEARCH: Page = {
+//   title: "Search",
+//   description: "Search all posts and projects by keyword.",
+// };
+
+// // Study Page
+// export const STUDIES = [
+//   {
+//     title: "Universitario en Desarrollo y Calidad del Software",
+//     institution: "UNSTA - Universidad del Norte Santo Tomás de Aquino",
+//     link: "https://www.unsta.edu.ar/",
+//     date: "2021 - 2024",
+//   },
+//   {
+//     title: "...",
+//     institution: "Platzi",
+//     link: "https://platzi.com/",
+//     date: "2018 - 2022",
+//   },
+//   {
+//     title: "Certificado Argentina Programa #SéProgramar",
+//     institution: "CESSI - Cámara de la Industria Argentina del Software",
+//     link: "https://cessi.org.ar/",
+//     date: "2018 - 2022",
+//   },
+//   {
+//     title: "Licenciatura en Cine y TV",
+//     institution: "Universidad Nacional de Córdoba",
+//     link: "https://www.unc.edu.ar/",
+//     date: "2017 - 2021",
+//   },
+// ];
+
+// export const EXPERIENCE = [
+//   {
+//     company: "Idea Software - Pyme",
+//     location: "Chaco, Argentina",
+//     position: "Software Engineer",
+//     start: "Junio - 2022",
+//     link: "",
+//     end: "Mayo - 2023",
+//     tasks: [
+//       "Integración de modulos en Odoo utilizando Python & React",
+//       "Pruebas y Depuración constantes",
+//     ],
+//   },
+//   {
+//     company: "No-Country",
+//     location: "Buenos Aires, Argentina",
+//     position: "Frontend Developer",
+//     link: "https://github.com/No-Country/",
+//     start: "2024",
+//     end: "Current",
+//     tasks: [
+//       "Desarrollo de la aplicación IFAC con el uso de React, Axios y TailwindCSS",
+//       "Interacción con los diferentes endpoints y Manejo de Errores",
+//       "Testing y Depuración",
+//     ],
+//   },
+//   {
+//     company: "UNSTA",
+//     link: "https://www.unsta.edu.ar/",
+//     location: "Yerba Brava Tucumán, Argentina",
+//     position: "Frontend developer",
+//     start: "2023",
+//     end: "Current",
+//     tasks: [
+//       "Desarrollo de la aplicación Appointment con el uso de React, Tanstack Query, y TailwindCSS",
+//       "Testing y Depuración",
+//     ],
+//   },
+// ];
